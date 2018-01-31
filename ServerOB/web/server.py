92395d191ea513bc3b88a06a3aa6b92956f725aa
williamfzc <178894043@qq.com>
2018-01-31 17:59:07 +0800

    add database & login module

--- ServerOB/web/server.py
+++ ServerOB/web/server.py
@@ -1,14 +1,22 @@
 # coding:utf-8
 from flask import Flask,render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from ..public import log_printer
 
 app = Flask(__name__)
 Bootstrap(app)
 
+# 用户验证
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+# http://blog.csdn.net/smalltankpy/article/details/53616053
+
 @app.route('/')
 def home():
-    return render_template('login.html', title_name = 'welcome')
+    return render_template('login.html')
 
 @app.route('/service')
 def service():
